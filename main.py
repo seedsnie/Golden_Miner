e7c5443e8e94c8028939d1a1d91a34e275cc1cb5
seedsnie <seedsnie@tencent.com>
2018-11-05 14:35:55 +0800

    富途demo

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+# 导入futuquant api
+import futuquant as ft
 
-def main():
-    pass
+# 实例化行情上下文对象
+quote_ctx = ft.OpenQuoteContext(host="127.0.0.1", port=11111)
 
-if __name__ == '__main__':
-    print('main enter')
-    main()
+# 上下文控制
+quote_ctx.start()              # 开启异步数据接收
+quote_ctx.set_handler(ft.TickerHandlerBase())  # 设置用于异步处理数据的回调对象(可派生支持自定义)
+
+# 低频数据接口
+market = ft.Market.HK
+code = 'HK.00123'
+code_list = [code]
+plate = 'HK.BK1107'
+print(quote_ctx.get_trading_days(market, start=None, end=None))   # 获取交易日
+print(quote_ctx.get_stock_basicinfo(market, stock_type=ft.SecurityType.STOCK))   # 获取股票信息
+print(quote_ctx.get_history_kline(code, start=None, end=None, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ))  # 获取历史K线
+print(quote_ctx.get_autype_list(code_list))                                  # 获取复权因子
+print(quote_ctx.get_market_snapshot(code_list))                              # 获取市场快照
+print(quote_ctx.get_plate_list(market, ft.Plate.ALL))                         # 获取板块集合下的子板块列表
+print(quote_ctx.get_plate_stock(plate))                         # 获取板块下的股票列表
