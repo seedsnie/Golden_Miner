6b3d7c75dea9d57715ae4138065c0d2cc28eea9b
seedsnie <seedsnie@tencent.com>
2018-11-08 19:57:02 +0800

    简单定义下加载历史数据接口和实现

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 # 导入futuquant api
 import futuquant as ft
+import pandas as pd
+import numpy as np
 
-# 实例化行情上下文对象
-quote_ctx = ft.OpenQuoteContext(host="127.0.0.1", port=11111)
 
-# 上下文控制
-quote_ctx.start()              # 开启异步数据接收
-quote_ctx.set_handler(ft.TickerHandlerBase())  # 设置用于异步处理数据的回调对象(可派生支持自定义)
+class StockDataLoader(object):
 
-# 低频数据接口
-market = ft.Market.HK
-code = 'HK.00123'
-code_list = [code]
-plate = 'HK.BK1107'
-print(quote_ctx.get_trading_days(market, start=None, end=None))   # 获取交易日
-print(quote_ctx.get_stock_basicinfo(market, stock_type=ft.SecurityType.STOCK))   # 获取股票信息
-print(quote_ctx.get_history_kline(code, start=None, end=None, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ))  # 获取历史K线
-print(quote_ctx.get_autype_list(code_list))                                  # 获取复权因子
-print(quote_ctx.get_market_snapshot(code_list))                              # 获取市场快照
-print(quote_ctx.get_plate_list(market, ft.Plate.ALL))                         # 获取板块集合下的子板块列表
-print(quote_ctx.get_plate_stock(plate))                         # 获取板块下的股票列表
+    def __init__(self):
+        # 实例化行情上下文对象
+        self.quote_ctx = ft.OpenQuoteContext(host="127.0.0.1", port=11111)  # type: ft.OpenQuoteContext
+        # 上下文控制
+        # self.quote_ctx.start()  # 开启异步数据接收
+        # self.quote_ctx.set_handler(ft.TickerHandlerBase())  # 设置用于异步处理数据的回调对象(可派生支持自定义)
+
+    def load_history(self):
+        code = 'HK.00700'
+        start = '2014-06-20'
+        end = '2014-06-30'
+        retdata = self.quote_ctx.get_history_kline(code, start=start, end=end, ktype=ft.KLType.K_DAY, autype=ft.AuType.QFQ)
+        retdata = retdata[1]  # type: pd.DataFrame
+        print(retdata.columns.values.tolist())
+        for row in np.asarray(retdata).tolist():
+            print(row)
+
+
+def main():
+    sdl = StockDataLoader()
+    sdl.load_history()
+
+
+if __name__ == '__main__':
+    main()
