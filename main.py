1b40038fe592ebf3ae49003be128131193eb4f02
seedsnie <815898739@qq.com>
2018-11-25 20:14:27 +0800

    扩大了特征数量，包括增加了各类常规指标特征，还有静态特征（包括市盈率，换手率），增加了不同模型的测试对比，增加了特征融合效果，将多天特征拼接到一起。 目前使用的统一归一化特征，效果不理想，准确率只能刚刚超过50%一点，后面思路有几个，一个是对特征做单独的归一化处理。二是模型考虑要使用深度学习模型，增大前后依赖特征效果

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import technicals as techs
 from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
+from sklearn import feature_selection
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
 
 class StockDataLoader(object):
@@ -25,6 +30,7 @@ class StockDataLoader(object):
 
         # code, time_key, open, close, high, low, pe_ratio ,turnover_rate,volume,turnover,change_rate,last_close
         self.dfquotes = retdata[1]  # type: pd.DataFrame
+        print('load quotes count=', self.dfquotes.shape[0])
 
     def get_quotes(self):
         return self.dfquotes
@@ -38,110 +44,236 @@ class StockFeatureExtract(object):
     def __init__(self, dfquotes, quote_params):
         self.dfquotes = dfquotes   # type: pd.DataFrame
         self.quote_params = quote_params  # type: dict
+        self.feature_cols = ['label']
 
     def extract(self):
         """
         根据股票行情数据提取特征数据
         """
+        self.extract_basics()
         self.extract_indexs()
         self.extract_labels()
 
     def get_features(self):
-        cols = ['feature_boll_upper', 'feature_boll_middle', 'feature_boll_lower',
-                'feature_macd_val', 'feature_macd_signal', 'feature_macd_hist',
-                'label']
-        dffeatures = self.dfquotes[cols]
+        print(self.feature_cols)
+        dffeatures = self.dfquotes[self.feature_cols]
         dffeatures = dffeatures.dropna()
-        return dffeatures
+        return dffeatures, self.feature_cols[1:]
+
+    def extract_basics(self):
+        """
+        提取股票的基础信息（市盈率，换手率等）
+        """
+        self.feature_cols.append('pe_ratio')
+        self.feature_cols.append('turnover_rate')
+        self.feature_cols.append('change_rate')
 
     def extract_indexs(self):
         """
         提取K线的一些常用量化指标特征（KDJ，MACD，RSI等）
         """
         dfquotes = self.dfquotes
+        dfhigh = dfquotes['high']
+        dflow = dfquotes['low']
+        dfclose = dfquotes['close']
+        dfvolume = dfquotes['volume']
 
         # BOLL
-        dfquotes['feature_boll_upper'], dfquotes['feature_boll_middle'], dfquotes['feature_boll_lower'] = \
-            techs.bbands(dfquotes.close.values)
+        dfboll_upper, dfboll_middle, dfboll_lower = techs.bbands(dfclose)
+        dfboll = (dfclose - dfboll_middle) / (dfboll_upper - dfboll_middle)
+        dfquotes['feature_boll'] = dfboll
+        self.feature_cols.append('feature_boll')
+
         # MACD
         dfquotes['feature_macd_val'], dfquotes['feature_macd_signal'], dfquotes['feature_macd_hist'] = \
-            techs.macd(dfquotes.close.values)
+            techs.macd(dfclose)
+        self.feature_cols.append('feature_macd_hist')
+        self.feature_cols.append('feature_macd_val')
+        self.feature_cols.append('feature_macd_signal')
 
-        # self.show_index_table()
+        # SAR
+        dfsar = techs.sar(dfquotes.high, dfquotes.low)
+        dfsar = (dfsar - dfclose) / dfclose
+        dfquotes['feature_sar'] = dfsar
+        self.feature_cols.append('feature_sar')
 
-    def show_index_table(self):
+        # 均线指标
+        # MA，EMA
+        timeperiods = [5, 10, 20, 30, 60, 120]
+        for timeperiod in timeperiods:
+            dfma = techs.ma(dfclose, timeperiod=timeperiod)
+            dfema = techs.ema(dfclose, timeperiod=timeperiod)
+            dfquotes['feature_ma' + str(timeperiod)] = dfma
+            dfquotes['feature_ema' + str(timeperiod)] = dfema
+            self.feature_cols.append('feature_ma' + str(timeperiod))
+            self.feature_cols.append('feature_ema' + str(timeperiod))
+
+        # 随机指标
+        # KD，RSI
+        dfkd_k, dfkd_d = techs.kd(dfhigh, dflow, dfclose)
+        dfquotes['feature_kd_k'] = dfkd_k
+        dfquotes['feature_kd_d'] = dfkd_d
+        self.feature_cols.append('feature_kd_k')
+        self.feature_cols.append('feature_kd_d')
+        timeperiods = [5, 10, 20, 30, 60, 120]
+        for timeperiod in timeperiods:
+            dfrsi = techs.rsi(dfclose, timeperiod=timeperiod)
+            dfquotes['feature_rsi' + str(timeperiod)] = dfrsi
+            self.feature_cols.append('feature_rsi' + str(timeperiod))
+
+        # 成交量指标
+        # AD，ADOSC，OBV
+        dfad = techs.ad(dfhigh, dflow, dfclose, dfvolume)
+        dfadosc = techs.adosc(dfhigh, dflow, dfclose, dfvolume)
+        dfobv = techs.obv(dfclose, dfvolume)
+        dfquotes['feature_ad'] = dfad
+        dfquotes['feature_adosc'] = dfadosc
+        dfquotes['feature_obv'] = dfobv
+        self.feature_cols.append('feature_ad')
+        self.feature_cols.append('feature_adosc')
+        self.feature_cols.append('feature_obv')
+
+        # 显示指标图表
+        # show_index = ['time_index', 'close', 'feature_sar']
+        # self.show_index_table(show_index)
+
+    def extract_labels(self):
+        dfquotes = self.dfquotes
+        dflabelcontexts = dfquotes['close'].to_frame()
+        dflabelcontexts['close_next'] = dflabelcontexts.shift(-1)
+
+        def apply_labels(close, close_next):
+            label = 1 if close < close_next else 0
+            return label
+        dfquotes['label'] = dflabelcontexts.apply(lambda row: apply_labels(row[0], row[1]), axis=1)
+        self.dfquotes = dfquotes[:-1]
+
+    def show_index_table(self, show_index):
         dfquotes = self.dfquotes
         x_datetime = pd.to_datetime(dfquotes.time_key)
         dfquotes['time_index'] = x_datetime
-        show_index = ['time_index', 'close',
-                      'feature_boll_upper', 'feature_boll_middle', 'feature_boll_lower',
-                      'feature_macd_hist']
         dfshow = dfquotes[show_index]
         ax = dfshow.plot(x='time_index', figsize=(20, 10), grid=True, title=self.quote_params['code'])
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # 設置x軸主刻度顯示格式（日期）
         ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))  # 設置x軸主刻度間距
         plt.show()
 
-    def extract_labels(self):
-        dfquotes = self.dfquotes
-        dflabelcontexts = dfquotes['close'].to_frame()
-        dflabelcontexts['close_next'] = dflabelcontexts.shift(1)
-
-        def apply_labels(close, close_next):
-            label = 1.0 if close < close_next else 0.0
-            return label
-        dfquotes['label'] = dflabelcontexts.apply(lambda row: apply_labels(row[0], row[1]), axis=1)
-        self.dfquotes = dfquotes[:-1]
-
 
 class MLFeatureProcess(object):
 
-    def __init__(self, dffeatures):
+    def __init__(self, dffeatures, feature_names):
         self.dffeatures = dffeatures   # type: pd.DataFrame
+        self.feature_names = feature_names
 
     def process(self):
         """
         将股票特征数据转化成符合机器学习规范的训练集和测试集
         :return: 数据集
         """
-        dfsamples = self.dffeatures.sample(frac=1.0)   # type: pd.DataFrame
-        dffeatures = dfsamples[['feature_boll_upper', 'feature_boll_middle', 'feature_boll_lower',
-                                'feature_macd_val', 'feature_macd_signal', 'feature_macd_hist']]
-        dflabel = dfsamples['label']
+        dffeatures, dflabels = self.feature_ensemble()
 
-        counts = dfsamples.shape[0]
-        train_split = 0.8
+        # 数据集划分
+        counts = dffeatures.shape[0]
+        train_split = 0.95
         train_index = int(round(train_split * counts))
-        train_x, test_x = dffeatures[:train_index].as_matrix(), dffeatures[train_index:].as_matrix()
-        train_y, test_y = dflabel[:train_index].as_matrix(), dflabel[train_index:].as_matrix()
-        dataset = {'train_x': train_x, 'test_x': test_x, 'train_y': train_y, 'test_y': test_y}
+        print('MLFeatureProcess samples total=%d train=%d test=%d' % (counts, train_index, (counts - train_index)))
+        train_x, test_x = dffeatures[:train_index].values, dffeatures[train_index:].values
+        train_y, test_y = dflabels[:train_index].values, dflabels[train_index:].values
+
+        # 数据归一化处理
+        train_x = preprocessing.scale(train_x)
+        test_x = preprocessing.scale(test_x)
+
+        dataset = {'train_x': train_x, 'test_x': test_x, 'train_y': train_y, 'test_y': test_y,
+                   'feature_names': self.feature_names}
 
         return dataset
+
+    def feature_ensemble(self):
+        """
+        特征融合（把多天特征融合到一个向量）
+        """
+        dffeatures = self.dffeatures[self.feature_names]
+        dflabel = self.dffeatures['label']
+        ensemble_days = 5
+        dffeatures_ensembles = [dffeatures]
+        for day in range(1, ensemble_days + 1):
+            dfconcat = dffeatures.shift(day)
+            feature_names_ago = [ name + '_ago' + str(day) for name in self.feature_names ]
+            dfconcat.columns = feature_names_ago
+            dffeatures_ensembles.append(dfconcat)
+        dffeatures_ensembles = pd.concat(dffeatures_ensembles, axis=1)
+        dffeatures_ensembles = dffeatures_ensembles[ensemble_days:]
+        dflabels = dflabel[ensemble_days:]
+        return dffeatures_ensembles, dflabels
 
 
 class MLModel(object):
 
     def __init__(self, dataset):
         self.dataset = dataset   # type: dict
-        self.classifier = LogisticRegression()
+        self.feature_selection = feature_selection.SelectPercentile(feature_selection.f_classif, percentile=50)
+        self.feature_selection_switch = False
+        # 模型配置
+        self.classifier_name = 'GradientBoostingClassifier'
+        if self.classifier_name == 'LogisticRegression':
+            self.classifier = LogisticRegression(verbose=1)
+        elif self.classifier_name == 'GradientBoostingClassifier':
+            # learning_rate=0.05, max_depth=3, min_samples_leaf=90, n_estimators=40, subsample=0.5
+            self.classifier = GradientBoostingClassifier(verbose=1, learning_rate=0.05, subsample=0.5, n_estimators=40)
+        elif self.classifier_name == 'SVC':
+            self.classifier = SVC()
 
     def train(self):
+        # 加载数据
         dataset = self.dataset
         train_x, train_y = dataset['train_x'], dataset['train_y']
-        self.classifier.fit(X=train_x, y=train_y)
+
+        # 特征筛选处理
+        train_x_fs = train_x
+        if self.feature_selection_switch:
+            train_x_fs = self.feature_selection.fit_transform(train_x, train_y)
+            select_features = self.feature_selection.get_support()  # type: np.ndarray
+            print(list(zip(self.dataset['feature_names'], select_features.tolist())))
+
+        # 模型训练
+        self.classifier.fit(X=train_x_fs, y=train_y)
+        # self.show_model()
+
+    def search_model(self):
+        # 加载数据
+        dataset = self.dataset
+        train_x, train_y = dataset['train_x'], dataset['train_y']
+        param_sets = {}
+        if self.classifier_name == 'GradientBoostingClassifier':
+            param_sets = {'n_estimators': range(20, 81, 10), 'subsample': np.arange(0.5, 1.0, 0.1),
+                          'max_depth': range(3, 14, 2), 'min_samples_split': range(100, 801, 200),
+                          'min_samples_leaf': range(60, 101, 10), 'learning_rate': [0.05, 0.1, 0.01]}
+        search = GridSearchCV(estimator=self.classifier, param_grid=param_sets, n_jobs=8)
+        search.fit(X=train_x, y=train_y)
+        print(search.best_params_, search.best_score_)
 
     def metrics(self):
         dataset = self.dataset
         test_x, test_y = dataset['test_x'], dataset['test_y']
-        pred_y = self.classifier.predict(X=test_x)
-        print(classification_report(y_true=test_y, y_pred=pred_y, target_names=['class_red', 'class_green']))
+        test_x_fs = test_x
+        if self.feature_selection_switch:
+            test_x_fs = self.feature_selection.transform(test_x)
+        pred_y = self.classifier.predict(X=test_x_fs)
+        print(classification_report(y_true=test_y, y_pred=pred_y, target_names=['class_green', 'class_red']))
+
+    def show_model(self):
+        if self.classifier_name == 'LogisticRegression':
+            print('\nshow model feature coef=', self.classifier.coef_)
+        elif self.classifier_name == 'GradientBoostingClassifier':
+            print('\nshow model feature importances=', self.classifier.feature_importances_)
 
 
 def main():
     # 输入参数
     input_code = 'HK.00700'
-    input_start = '2018-01-01'
-    input_end = '2018-11-09'
+    input_start = '2000-02-08'
+    input_end = '2018-11-24'
 
     # 加载历史数据
     sdl = StockDataLoader(input_code, input_start, input_end)
@@ -152,7 +284,8 @@ def main():
     sfe.extract()
 
     # 模型训练和效果评估
-    mlfp = MLFeatureProcess(sfe.get_features())
+    dffeatures, feature_names = sfe.get_features()
+    mlfp = MLFeatureProcess(dffeatures, feature_names)
     dataset = mlfp.process()
     model = MLModel(dataset)
     model.train()
--- technicals.py
+++ technicals.py
@@ -1,10 +1,10 @@
 import talib
 
 
-def bbands(closes=None, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0):
+def bbands(close=None, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0):
     """
     BOLL（林线指标）
-    :param closes: 收盘价
+    :param close: 收盘价
     :param timeperiod: 时间周期
     :param nbdevup: 上轨线标准差倍数
     :param nbdevdn: 下轨线标准差倍数
@@ -12,20 +12,145 @@ def bbands(closes=None, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0):
         0=SMA, 1=EMA, 2=WMA, 3=DEMA, 4=TEMA, 5=TRIMA, 6=KAMA, 7=MAMA, 8=T3 (Default=SMA)
     :return: 上布林，中布林，下布林
     """
-    upper, middle, lower = talib.BBANDS(closes, timeperiod=timeperiod, nbdevup=nbdevup, nbdevdn=nbdevdn, matype=matype)
+    upper, middle, lower = talib.BBANDS(close, timeperiod=timeperiod, nbdevup=nbdevup, nbdevdn=nbdevdn, matype=matype)
     return upper, middle, lower
 
 
-def macd(closes=None, fastperiod=12, slowperiod=26, signalperiod=9):
+def macd(close=None, fastperiod=12, slowperiod=26, signalperiod=9):
     """
     MACD（移动平均收敛/散度）
     指标解读：核心是看macdhist，柱子由正变负代表卖出信号，柱子由负变正代表买入信号
-    :param closes: 收盘价
+    :param close: 收盘价
     :param fastperiod: 短周期
     :param slowperiod: 长周期
     :param signalperiod: 移动平均周期
     :return: macd值，macd9日平均值，macd直方图高度
     """
-    macd, macdsignal, macdhist = talib.MACD(closes, fastperiod=fastperiod, slowperiod=slowperiod,
+    macd, macdsignal, macdhist = talib.MACD(close, fastperiod=fastperiod, slowperiod=slowperiod,
                                             signalperiod=signalperiod)
     return macd, macdsignal, macdhist
+
+
+def ma(close=None, timeperiod=5, matype=0):
+    """
+    MA（移动平均线）
+    :param close: 收盘价
+    :param timeperiod: 时间周期
+    :param matype: 移动平均类型
+        0=SMA, 1=EMA, 2=WMA, 3=DEMA, 4=TEMA, 5=TRIMA, 6=KAMA, 7=MAMA, 8=T3 (Default=SMA)
+    :return: MA平均线值
+    """
+    maval = talib.MA(close, timeperiod=timeperiod, matype=matype)
+    return maval
+
+
+def kd(high, low, close, fastk_period=9, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0):
+    """
+    KDJ（随机指标）
+    :param high: 最高价
+    :param low: 最低价
+    :param close: 收盘价
+    :param fastk_period: 快速随机线K周期
+    :param slowk_period: 慢速随机线K周期
+    :param slowk_matype: 移动平均类型
+    :param slowd_period: 慢速随机线d周期
+    :param slowd_matype: 移动平均类型
+    :return: 快速线K，慢速线D
+    """
+    k, d = talib.STOCH(high, low, close, fastk_period=fastk_period, slowk_period=slowk_period,
+                       slowk_matype=slowk_matype, slowd_period=slowd_period, slowd_matype=slowd_matype)
+    return k, d
+
+
+def rsi(close, timeperiod=14):
+    """
+    RSI（相对强弱指标）
+    :param close: 收盘价
+    :param timeperiod: 移动平均周期
+    :return: RSI值
+    """
+    rsival = talib.RSI(close, timeperiod=timeperiod)
+    return rsival
+
+
+def mavol(volume=None, timeperiod=5, matype=0):
+    """
+    MA（成交量移动平均线）
+    :param volume: 成交量
+    :param timeperiod: 时间周期
+    :param matype: 移动平均类型
+        0=SMA, 1=EMA, 2=WMA, 3=DEMA, 4=TEMA, 5=TRIMA, 6=KAMA, 7=MAMA, 8=T3 (Default=SMA)
+    :return: MAVOL平均线值
+    """
+    maval = talib.MA(volume, timeperiod=timeperiod, matype=matype)
+    return maval
+
+
+def ema(close=None, timeperiod=5):
+    """
+    MA（指数移动平均线）
+    :param close: 收盘价
+    :param timeperiod: 时间周期
+    :return: EMA平均线值
+    """
+    maval = talib.EMA(close, timeperiod=timeperiod)
+    return maval
+
+
+def sar(high, low, acceleration=2, maximum=20):
+    """
+    SAR（停损转向操作点指标）
+    (1)当股价上涨时,SAR的红色圆圈位于股价的下方,当该股的收盘价向下跌破SAR时,则应立即停损卖出。
+    (2)当股价下跌时,SAR的绿色圆圈位于股价的上方,当收盘价向上突破SAR时,可以重新买回。
+    :param high: 最高价
+    :param low: 最低价
+    :param acceleration: 加速因子步长
+    :param maximum: 最大值
+    :return: SAR值
+    """
+    sarval = talib.SAR(high, low, acceleration=acceleration, maximum=maximum)
+    return sarval
+
+
+def ad(high, low, close, volume):
+    """
+    AD（多空双方力量浮标）
+    :param high: 最高价
+    :param low: 最低价
+    :param close: 收盘价
+    :param volume: 成交量
+    :return: AD值
+    """
+    adval = talib.AD(high, low, close, volume)
+    return adval
+
+
+def adosc(high, low, close, volume, fastperiod=3, slowperiod=10):
+    """
+    ADOSC（收集派发摆荡指标）
+    AD LINE的３天EMA值与10天EMA值的差值。这个指标是一个以０为中心的摆荡指标。这种计算手法与MACD的计算方法相似。
+    :param high: 最高价
+    :param low: 最低价
+    :param close: 收盘价
+    :param volume: 成交量
+    :param fastperiod: 快线周期
+    :param slowperiod: 慢线周期
+    :return: ADOSC值
+    """
+    adoscval = talib.ADOSC(high, low, close, volume, fastperiod=fastperiod, slowperiod=slowperiod)
+    return adoscval
+
+
+def obv(close, volume):
+    """
+    OBV（能量潮指标）
+    （1）当股价上升而OBV线下降，表示买盘无力，股价可能会回跌。
+    （2）股价下降时而OBV线上升，表示买盘旺盛，逢低接手强股，股价可能会止跌回升。
+    （3）OBV线缓慢上升，表示买气逐渐加强，为买进信号。
+    （4）OBV线急速上升时，表示力量将用尽为卖出信号
+    :param close:
+    :param volume:
+    :return:
+    """
+    obvval = talib.OBV(close, volume)
+    return obvval
